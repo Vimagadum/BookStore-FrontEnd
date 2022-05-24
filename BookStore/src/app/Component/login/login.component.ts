df2d5e4eb4b2e1a8d37f8811183086b00a96c3cb
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-05-24 16:07:54 +0530

    [Vinayak] added login page

--- BookStore/src/app/Component/login/login.component.ts
+++ BookStore/src/app/Component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/Service/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  submitted = false;
+
+  category: any;
+  
+  constructor(private formBuilder: FormBuilder, private user:UserService, private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    }); 
   }
-
-}
+  onSubmitloginForm(){
+    this.submitted=true;
+    console.log("inputs", this.loginForm.value);
+    if(this.loginForm.valid){
+      console.log("valid",this.loginForm.value);
+      let data= {
+          email:this.loginForm.value.email,
+          password:this.loginForm.value.password,
+      }
+    }
+  }}
